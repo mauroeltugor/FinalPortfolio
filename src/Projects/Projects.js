2a4df1902a703b4b22778dba043dbca53098df3c
mauro <andrekxus299@gmail.com>
2023-09-15 10:57:07 -0500

    fix images

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,31 +1,26 @@
-import Sms from '../../public/images/Sms.png';
-import Blue from '../../public/images/Blueeasy.png';
-import Bleux from '../../public/images/Bleux.png';
-import Cripto from '../../public/images/cripto.png';
-
 export const data = [
     {
         id: 1,
         link: "https://polite-phoenix-da56ba.netlify.app",
         nombre: "SMS",
-        imagen: Sms,
+        imagen: "https://i.ibb.co/WvvzKxP/Sms.png",
     },
     {
         id: 2,
         link: "https://storied-douhua-ef3b2b.netlify.app",
         nombre: "Blue easy",
-        imagen: Blue,
+        imagen: "https://i.ibb.co/gPYjLR5/Blueeasy.png",
     },
     {
         id: 3,
         link: "https://playful-beignet-a6e231.netlify.app",
         nombre: "Bleux",
-        imagen: Bleux,
+        imagen: "https://i.ibb.co/bXGhdSr/Bleux.png",
     },
     {
         id: 4,
         link: "https://vocal-haupia-2d350a.netlify.app",
         nombre: "Cripto coin",
-        imagen: Cripto,
+        imagen: "https://i.ibb.co/ZNtCQmn/cripto.png",
     },
 ];
