92b1fe6a4e1a923b421778eb06b0f0a710af2362
mauro <andrekxus299@gmail.com>
2024-01-31 11:01:39 -0500

    delete tittles and remove ecomerce

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,7 +4,6 @@ import { data } from '../../Projects/Projects';
 export const Projects = () => {
   return (
     <div id='projects' className='h-screen'>
-      <h1>Proyectos</h1>
       <div className="flex flex-wrap">
         {data.map((project) => (
           <div key={project.id} className="max-w-md m-4 bg-white shadow-lg rounded-lg overflow-hidden">
